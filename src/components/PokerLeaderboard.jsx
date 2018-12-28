3581b79a7e2e872b0be26b951a17a60784100236
Max Cohen <cohenmax9@gmail.com>
2018-12-27 18:45:28 -0700

    Initial commit.

---
+++ src/components/PokerLeaderboard.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class PokerLeaderboard extends Component {
+  render() {
+    return <div className="poker-leaderboard">Hello world</div>;
+  }
+}
